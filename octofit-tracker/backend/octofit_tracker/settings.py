845551f6140820e12d38dce8100602e1bd916e23
Rajeev Ojha <69325206+rajeevojha@users.noreply.github.com>
2025-04-13 08:19:26 +0000

    fixed django issue

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
+        'ENGINE': 'django',
         'NAME': 'octofit_db',
     }
 }
